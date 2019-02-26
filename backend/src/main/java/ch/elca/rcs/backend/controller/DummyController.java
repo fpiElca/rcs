80314cd3734f4d673290ad3deed7eab62ca19e65
Frederic Pillonel <fpi.elca@gmail.com>
2019-02-26 17:12:32 +0100

    01 - Enhancing swagger example

--- backend/src/main/java/ch/elca/rcs/backend/controller/DummyController.java
+++ backend/src/main/java/ch/elca/rcs/backend/controller/DummyController.java
@@ -1,14 +1,40 @@
 package ch.elca.rcs.backend.controller;
 
+import ch.elca.rcs.backend.domain.DummyEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value="dummyctrl", description="Just a sample controller for demo purposes")
 public class DummyController {
 
+    @ApiOperation(value = "Returns some dummy text", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully returned the text"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Operation not authorized"),
+            @ApiResponse(code = 404, message = "Resource not found")
+    })
     @RequestMapping(value = "/dummy", method = RequestMethod.POST)
     public String dummy() {
         return "dummy";
+    }
+
+    @ApiOperation(value = "Returns some dummy text", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully returned the text"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Operation not authorized"),
+            @ApiResponse(code = 404, message = "Resource not found")
+    })
+
+    @RequestMapping(value = "/sendDummy", method = RequestMethod.POST)
+    public void sendDummy(@RequestBody DummyEntity sendDummy) {
     }
 }
--- backend/src/main/java/ch/elca/rcs/backend/domain/DummyEntity.java
+++ backend/src/main/java/ch/elca/rcs/backend/domain/DummyEntity.java
@@ -1,12 +1,20 @@
 package ch.elca.rcs.backend.domain;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class DummyEntity {
 
-    private @Id @GeneratedValue Long id;
+    @Id @GeneratedValue
+    @ApiModelProperty(notes = "This is just an ID", required = true, allowableValues = "0, 1, 2")
+    private Long id;
+
+    @ApiModelProperty(notes = "The description of this instance")
     private String description;
 }
