08571c3a6e8798588945ab57bb5d2580b82006d2
Heinzer Stefan <stefan.heinzer@elca.ch>
2019-02-27 17:36:35 +0100

    1 - added unit tests for Haltestelle repo

--- backend/src/main/java/ch/elca/rcs/backend/BackendApplication.java
+++ backend/src/main/java/ch/elca/rcs/backend/BackendApplication.java
@@ -2,9 +2,20 @@ package ch.elca.rcs.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.hateoas.hal.CurieProvider;
+import org.springframework.hateoas.hal.DefaultCurieProvider;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	public static String CURIE_NAMESPACE = "rcs";
+
+	// @Bean // TODO: enable, adapt WebIntegration test and frontend
+	public CurieProvider curieProvider() {
+		return new DefaultCurieProvider(CURIE_NAMESPACE, new UriTemplate("/docs/{rel}.html"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
